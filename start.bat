85c48605ba8844e31a87a527a92f5bed924eb7d2
Islam Vadimov <139468035+Lp1xS@users.noreply.github.com>
2023-08-13 14:16:19 +0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,11 @@
+|
+ # Сделано для некоторых даунов, котороы не могут сделать свое
+ # Предназначено для Windows, чтоб запустить > закидываешь в папку сборки и открываешь данный .bat, главное укажите название ядра правильно
+
 @ECHO OFF
 color 7
-title BungeeCord
+title Auth
 echo  /
 chcp 1251
-java -Xms2024M -Xmx2024M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar velocity.jar
+java -Xms2024M -Xmx2024M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar velocity.jar 
 pause
